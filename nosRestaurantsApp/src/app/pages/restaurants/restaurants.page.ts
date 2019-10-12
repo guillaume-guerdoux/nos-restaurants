437397701fe44113bcb858adcae279ea85994f2e
Guillaume <guillaume.guerdoux@hotmail.fr>
2019-10-12 13:29:20 +0200

    Begin backend

--- nosRestaurantsApp/src/app/pages/restaurants/restaurants.page.ts
+++ nosRestaurantsApp/src/app/pages/restaurants/restaurants.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleService } from './../../services/google-services.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-restaurants',
@@ -37,9 +40,49 @@ export class RestaurantsPage implements OnInit {
     'opening_hours': {'open_now': true}, 'plus_code': {'global_code': '8FHFXCX3+GR', 'compound_code': 'XCX3+GR Prunelli-di-Fiumorbo, France'},
     'reference': 'ChIJRRudyITv2RIRWCzyBL-UiYI', 'user_ratings_total': 20, 'name': 'Le Mondo', 'vicinity': 'Route de Calzarellu, Prunelli-di-Fiumorbo'}];
 
-  constructor() { }
+  coordinates = null;
+  address = null;
+  min_rate = 0;
+  min_nb_reviews = 0;
+  is_open = true;
+  restaurants_results: Observable<any>;
+  api_key = "AIzaSyBMcrzDkykbrg8iIoXnkmWVxdFmBcg48V0"
+
+  constructor(private googleService: GoogleService) { }
 
   ngOnInit() {
+    // Get the geolocalisaition information from the API
+    this.googleService.getGeolocalisation().subscribe(result => {
+      this.coordinates = result['location'];
+      this.googleService.getAdressFromGPS(this.coordinates.lat, this.coordinates.lng).subscribe(result => {
+        console.log(result)
+        this.address = result['results'][0].formatted_address
+      })
+    });
+  }
+
+
+  launchRestaurantSearch() {
+    this.googleService.getRestaurants(String(this.coordinates.lat) + ',' + String(this.coordinates.lng), this.is_open)
+      .subscribe(result => {
+        var temp_restaurants = result['results'];
+        var final_restaurants = {}
+        /*if (min_rate = 0 and min_nb_reviews = 0) {
+          this.restaurants_results = temp_restaurants
+        }
+        else {
+          for (var restaurant in temp_restaurants) {
+            if ()
+          }
+        }*/
+
+        this.restaurants_results = result['results'];
+        console.log(this.restaurants_results)
+      });
+  }
+
+  consoleChanged() {
+    console.log('changed')
   }
 
 }
--- nosRestaurantsApp/src/app/services/google-services.service.ts
+++ nosRestaurantsApp/src/app/services/google-services.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+import { HTTP } from '@ionic-native/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GoogleServicesService {
+export class GoogleService {
 
-  constructor() { }
+  googleGeolocUrl = 'https://www.googleapis.com/geolocation/v1/geolocate'
+  googleGeolocAddressUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
+  googleGetPlacesUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+  apiKey = 'AIzaSyBMcrzDkykbrg8iIoXnkmWVxdFmBcg48V0'
+
+  /**
+  * Constructor of the Service with Dependency Injection
+  * @param http The standard Angular HttpClient to make requests
+  */
+  constructor(private http: HttpClient) { }
+
+  /**
+  * Get GPS coordinates
+  * @returns Observable with detailed GPS information
+  */
+  getGeolocalisation() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        "Accept": 'application/json',
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post(`${this.googleGeolocUrl}?key=${this.apiKey}`, {}, httpOptions);
+  }
+  /**
+  * Get Address from GPS coordinates
+  * @returns Observable with detailed Adresse information
+  */
+  getAdressFromGPS(lat, lng) {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        "Accept": 'application/json',
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post(`https://cors-anywhere.herokuapp.com/${this.googleGeolocAddressUrl}?latlng=${lat},${lng}&key=${this.apiKey}`, {}, httpOptions);
+  }
+
+  /**
+  * Get restaurants from the google maps
+  * map the result to return only the results that we need
+  *
+  * @param {string} location lat, long
+  * @param {boolean} is_open
+  * @returns Observable with the search results
+  */
+  getRestaurants(location: string, is_open: boolean): Observable<any> {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        "Accept": 'application/json',
+        'Content-Type':  'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT'
+      })
+    };
+    if (is_open) {
+      return this.http.get(`https://cors-anywhere.herokuapp.com/${this.googleGetPlacesUrl}?location=${location}&rankby=distance&type=restaurant&opennow&key=${this.apiKey}`, httpOptions)
+    }
+    else {
+      return this.http.get(`https://cors-anywhere.herokuapp.com/${this.googleGetPlacesUrl}?location=${location}&rankby=distance&type=restaurant&key=${this.apiKey}`, httpOptions)
+    }
+  }
 }
